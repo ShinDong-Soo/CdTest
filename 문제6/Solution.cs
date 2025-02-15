33757efef4c0673dd59aa39edefc99f3baa18d51
ShinDong-Soo <sh0819su@gmail.com>
2025-02-16 01:42:01 +0900

    Create Solution.cs

---
+++ 문제6/Solution.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string[] s
+            
+        s = Console.ReadLine().Split('');
+        
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s((1));
+        
+        Console.WriteLine($"{a} + {b} = {a + b}");
+    }
+}
+
+                            
