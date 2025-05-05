10b8437239181470a9777267cb34eb62bb695e07
ShinDong-Soo <sh0819su@gmail.com>
2025-05-05 21:00:56 +0900

    Update Solution.cs

--- 문제3/Solution.cs
+++ 문제3/Solution.cs
@@ -1,26 +1,16 @@
-using System;
+문자열 반복해서 출력하기
+제출 내역
+문제 설명
+문자열 str과 정수 n이 주어집니다.
+str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
 
-public class Example
-{
-    public static void Main()
-    {
-        String[] input;
+제한사항
+1 ≤ str의 길이 ≤ 10
+1 ≤ n ≤ 5
+입출력 예
+입력 #1
 
-        Console.Clear();
-        input = Console.ReadLine().Split(' ');
+string 5
+출력 #1
 
-        String s1 = input[0];
-        int a = Int32.Parse(input[1]);
-        
-        for (i > 0;, i <= a,i++;)
-        {    
-            Console.WriteLine($"{s1}{s1 * i}")
-        }
-    }
-}
-
-
-문제점:
-
-문자열을 반복할 수 있는 함수로 활용해야 한다는 생각을 하지 못함.
-
+stringstringstringstringstring
